cd1c79a2a0c2895287d5f5956f3c3e152a75859e
Tanay Raj <tanay.raj76@gmail.com>
2025-07-16 22:53:17 +0530

    add go config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,15 @@
 return {
   { "mason-org/mason.nvim", opts = {} },
   {
+    'ray-x/go.nvim',
+    requires = {
+      'ray-x/guihua.lua'
+    },
+    config = function()
+      require("config/go")
+    end
+  },
+  {
     "mason-org/mason-lspconfig.nvim",
     opts = {
       automatic_enable = {
@@ -15,7 +24,7 @@ return {
     },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd" },
+        ensure_installed = { "lua_ls", "clangd", "gopls" },
       })
     end,
   },
@@ -36,6 +45,55 @@ return {
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      lspconfig.gopls.setup({
+        cmd = { "gopls" },
+        capabilities = capabilities,
+        -- for postfix snippets and analyzers
+        settings = {
+          gopls = {
+            buildFlags = { "-tags=wireinject" },
+            experimentalPostfixCompletions = true,
+            analyses = {
+              unusedparams = true,
+              shadow = true,
+              nilness = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            codelenses = {
+              gc_details = false,
+              generate = true,
+              regenerate_cgo = true,
+              run_govulncheck = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
+            },
+            staticcheck = true,
+            semanticTokens = true,
+            ["ui.inlayhint.hints"] = {
+              compositeLiteralFields = true,
+              constantValues = true,
+              parameterNames = true,
+              functionTypeParameters = true,
+              compositeLiteralTypes = true,
+              assignVariableTypes = true,
+            },
+            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+            usePlaceholders = true,
+          },
+        },
+      })
+
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require("go.format").goimports()
+        end,
+        group = format_sync_grp,
+      })
 
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "java",
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,25 @@
 return {
 
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.google_java_format,
-				null_ls.builtins.diagnostics.checkstyle.with({
-					extra_args = { "-c", "/google_checks.xml" },
-				}),
-				require("none-ls.diagnostics.eslint"),
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.google_java_format,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.diagnostics.checkstyle.with({
+          extra_args = { "-c", "/google_checks.xml" },
+        }),
+        require("none-ls.diagnostics.eslint"),
+      },
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
